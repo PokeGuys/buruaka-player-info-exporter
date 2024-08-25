798acfed16eccd9577d99885e629e194f900ed71
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2024-08-25 09:44:37 -0700

    fix: cannot compress json using lzstring (#5)

--- scripts/armory_converter.py
+++ scripts/armory_converter.py
@@ -1,9 +1,11 @@
 import json
-import lzstring
+from lzstring import LZString
 
 
-with open("./templates/armory_template.json", "r", encoding="utf-8") as f:
-    ARMORY_TEMPLATE = json.load(f)
+with open("./armory_code.txt", "r", encoding="utf-8") as f:
+    code = f.read()
+    print(LZString.decompressFromBase64(code))
+    ARMORY_TEMPLATE = json.loads(LZString.decompressFromBase64(code))
 
 
 def restore_skill(level, type="O"):
@@ -34,25 +36,32 @@ with open("./output/characters.json", "r", encoding="utf-8") as f:
             for skill_idx, skill in enumerate(skills)
         ]
         ARMORY_TEMPLATE["students"][idx]["level"] = target["level"]
-        ARMORY_TEMPLATE["students"][idx]["levelTarget"] = target["level"]
+        ARMORY_TEMPLATE["students"][idx]["levelTarget"] = max(
+            ARMORY_TEMPLATE["students"][idx]["levelTarget"], target["level"]
+        )
         ARMORY_TEMPLATE["students"][idx]["star"] = target["starGrade"]
-        ARMORY_TEMPLATE["students"][idx]["starTarget"] = target["starGrade"]
+        ARMORY_TEMPLATE["students"][idx]["starTarget"] = max(
+            ARMORY_TEMPLATE["students"][idx]["starTarget"], target["starGrade"]
+        )
         ARMORY_TEMPLATE["students"][idx]["weapon"] = target["ueGrade"]
-        ARMORY_TEMPLATE["students"][idx]["weaponTarget"] = target["ueGrade"]
+        ARMORY_TEMPLATE["students"][idx]["weaponTarget"] = max(
+            target["ueGrade"], ARMORY_TEMPLATE["students"][idx]["weaponTarget"]
+        )
         ARMORY_TEMPLATE["students"][idx]["gear"] = (
             0 if target["uniqueItem"] == "N/A" else target["uniqueItem"]
         )
-        ARMORY_TEMPLATE["students"][idx]["gearTarget"] = (
-            0 if target["uniqueItem"] == "N/A" else target["uniqueItem"]
+        ARMORY_TEMPLATE["students"][idx]["gearTarget"] = max(
+            ARMORY_TEMPLATE["students"][idx]["gearTarget"],
+            0 if target["uniqueItem"] == "N/A" else target["uniqueItem"],
         )
         ARMORY_TEMPLATE["students"][idx]["equipments"] = [
             {
                 "studentId": s["id"],
                 "index": eq_idx,
                 "tier": target[f"equipment_{eq_idx + 1}"],
-                "tierTarget": target[f"equipment_{eq_idx + 1}"],
+                "tierTarget": max(target[f"equipment_{eq_idx + 1}"], eq["tierTarget"]),
             }
-            for eq_idx, _ in enumerate(ARMORY_TEMPLATE["students"][idx]["equipments"])
+            for eq_idx, eq in enumerate(ARMORY_TEMPLATE["students"][idx]["equipments"])
         ]
 with open("./output/items.json", "r", encoding="utf-8") as f:
     items = json.load(f)
@@ -61,9 +70,10 @@ with open("./output/equipments.json", "r", encoding="utf-8") as f:
     equipments = json.load(f)
     for eq in equipments:
         ARMORY_TEMPLATE["stocks"][2000000 + eq["id"]] = eq["count"]
-lz = lzstring.LZString()
-with open("./armory_code.txt", "w", encoding="utf-8") as f:
+with open("./armory_code.out.txt", "w", encoding="utf-8") as f:
     f.write(
-        lz.compressToBase64(json.dumps(ARMORY_TEMPLATE, indent=4, ensure_ascii=False))
+        LZString.compressToBase64(
+            json.dumps(ARMORY_TEMPLATE, indent=4, ensure_ascii=False)
+        )
     )
     print("Done")
